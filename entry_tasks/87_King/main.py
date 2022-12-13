163a34124e1edfd94a930e644a2c1ce4d71af2d2
Julia Fishjuk <juliafishjuk1985@gmail.com>
2022-12-13 18:08:08 +0200

    Fix:King input

---
+++ entry_tasks/87_King/main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print('Kings name:')
+    x = input()
+    print('Greetings,' + x)
+    print('What do you want to say to the people of our great Kingdom?:')
+    x = input()
+    print('The king says:, ' + x)
