1de6a637de285574f1641cc379ca8c8469625d41
Ilona <ilonaokrepka@gmail.com>
2022-12-20 11:35:37 +0200

    feat:secret_counter #91

---
+++ entry_tasks/91_secret_counter/91_secret_counter.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    long_text = input('Enter the long text: ')
+    secret_word = input('Enter the secret word: ')
+    print((long_text.count(secret_word)),'secret word(s) in your input')
