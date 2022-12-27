9a2f45f725a96c4495b15b3b4950e0a0b7baf6f5
viktoriazinoveva <zinovieva1506@gmail.com>
2022-12-27 19:44:01 +0100

    fix: comment #97

--- entry_tasks/movie_picker/movie_picker.py
+++ entry_tasks/movie_picker/movie_picker.py
@@ -1,4 +1,5 @@
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     GENRES = {
         'comedy': ['Meet the Parents', 'Anger Management'],
         'adventures': ['Mummy'],
@@ -8,59 +9,48 @@ if __name__ == "__main__":
         'action': ['Mission Impossible']
     }
 
-    ACTORS = {
-        'Robert De Niro': ['Meet the Parents'],
-        'Ben Stiller': ['Meet the Parents'],
-        'Adam Sandler': ['Anger Management'],
-        'Jack Nicholson': ['Anger Management'],
-        'Brendan Fraser': ['Mummy'],
-        'Rachel Weisz': ['Mummy'],
-        'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
-        'Penelope Cruz': ['Vanilla Sky'],
-        'Cameron Diaz': ['Vanilla Sky'],
-        'Brad Pitt': ['Meet Joe Black'],
-        'Anthony Hopkins': ['Meet Joe Black'],
-        'Jeremy Renner': ['Mission Impossible']
+    CAST = {
+        'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+        'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+        'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+        'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+        'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+        'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
     }
 
 
-    def search_by_genre_or_actor(search_type):
-        if search_type == "genre":
-            all_items = ", ".join(list(GENRES.keys()))
-            item_type = "genre"
-            items_dict = GENRES
-        elif search_type == "actor":
-            all_items = ", ".join(list(ACTORS.keys()))
-            item_type = "actor"
-            items_dict = ACTORS
+    def search(all_items, search_type):
+        print(f"{'Available '}{search_type}s: {all_items}")
+        user_choose_item = input(f"Enter {search_type}: ").strip()
+        while user_choose_item not in all_items:
+            user_choose_item = input(f"Enter {search_type}: ").strip()
         else:
-            print("Incorrect search type. Please enter either 'genre' or 'actor'.")
-            return
-
-        print(f"{'Available '}{item_type.capitalize()}s: {all_items}")
-        user_choose_item = input(f"Enter {item_type}: ").strip()
-        if user_choose_item in items_dict:
-            movies_list = items_dict[user_choose_item]
-            print(f'Available movies: {movies_list}')
-            user_choose_movie = input('Enter movie: ').strip()
-            if user_choose_movie in items_dict[user_choose_item]:
-                print(
-                    f'Movie to watch: {user_choose_movie}. Starring: {user_choose_item}.' if search_type == "actor" else
-                    f'Movie to watch: {user_choose_movie}. Genre: {user_choose_item}.')
-            else:
-                print('Incorrect movie name! Try again')
-        else:
-            print(f'Incorrect {item_type} name! Try again')
+            return user_choose_item
 
 
-    answer = input("Search by genre: ").strip()
-    if answer == 'y':
-        search_by_genre_or_actor('genre')
-    elif answer == 'n':
-        actor = input("Search by actor: ").strip()
-        if actor == 'y':
-            search_by_genre_or_actor('actor')
-        elif answer == 'n':
-            print('I really do not understand what you want!')
-    else:
-        print("Invalid input. Please enter 'y' or 'n'. Try again!")
+    def movies_by_actors(cast):
+        inverted_dict = {}
+        for movie, actors in cast.items():
+            for actor in actors:
+                inverted_dict[actor] = inverted_dict.get(actor, []) + [movie]
+        return inverted_dict
+
+
+    by_genre = input('Search by Genre: ').lower() == 'y'
+
+    if by_genre:
+        genre = search(all_items=list(GENRES.keys()), search_type='genre')
+        movie = search(all_items=GENRES[genre], search_type='movie')
+        print(f'Movie to watch: {movie}. Genre: {genre}.')
+        exit()
+
+    by_actor = input('Search by Actor: ').lower() == 'y'
+
+    if by_actor:
+        actors = movies_by_actors(CAST)
+        actor = search(all_items=list(actors.keys()), search_type='actor')
+        movie = search(all_items=actors[actor], search_type='movie')
+        print(f'Movie to watch: {movie}. Starring: {actor}.')
+        exit()
+
+    print('Search available by Genre or Actor only. Please try again.')
