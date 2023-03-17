bff9928909711ffcf22ede1e063d2f0bc7012936
Andriy Havryshkevych <gavandre@gmail.com>
2023-03-17 23:54:13 +0200

    input program v1 , still need review and adjustments

---
+++ week_1/sum_numbers/sum_numbers.py
@@ -0,0 +1,26 @@
+
+
+while True:
+    while True:
+        x = input("Please enter your first number: ")
+        if x.isdigit():
+            while True:
+                y = input("please enter you second number: ")
+                if y.isdigit():
+                    print(f"The sum of you number is {int(x) + int(y)}")
+                    break
+                else:
+                    print("it seems you've entered wrong symbols, please try again< please note you have type digits")
+                    continue
+        else:
+            print("it seems you've entered wrong symbols, please try again< please note you have type digits")
+            continue
+        break
+    exit = input("To to proceed, press any button, if you want to exit the program please press 'E', : ")
+    if exit == "E" or "e":
+        break
+    else:
+        continue
+
+
+
