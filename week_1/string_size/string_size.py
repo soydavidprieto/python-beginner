cae5237c5191ad2bfe516bfb97f1eac8710fa583
Andriy Havryshkevych <gavandre@gmail.com>
2023-03-18 16:14:38 +0200

    minor fixes

--- week_1/string_size/string_size.py
+++ week_1/string_size/string_size.py
@@ -1,7 +1,7 @@
 while True:
     while True:
         s = input("Please enter your string: ")
-        print(f"The stiring you've entered is '{s}', and it's lenghts is '{len(s)}'")
+        print(f"The string you've entered is '{s}', and it's git add lengths is '{len(s)}'")
         break
     exit = input("To to proceed, press any button, if you want to exit the program please press 'E', : ")
     if exit == "E" or exit == "e":
