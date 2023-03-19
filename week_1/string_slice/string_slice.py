ee7c43dc847e2c84e6587206984ab47fe6d787bb
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-19 17:53:04 +0100

    add program which slice string

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+
+    s = input('Enter something: ')
+    start = int(input('Enter start index: '))
+    end = int(input('Enter end index:  '))
+    print(s)
+
+    p = s[start:end]
+    print(p)
