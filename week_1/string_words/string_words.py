0de1f19153caf730f0aa29431d558772a1bfe70d
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-19 17:16:58 +0100

    add program which splits text into words

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 6. String words')
+f = "It is better to take many small steps\
+in the right direction than to make a great leap\
+forward only to stumble backward."
+print(f)
