b472ce536023515da877da913e075a65236e7c0d
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-20 23:21:53 +0200

    change request (task 7)

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -6,3 +6,7 @@ sub_string = string_slice[0::2]
 print(sub_string)
 
 #to be honest I haven't understood the task since it has very unclear explanation so I've just played around with slicing
+# TODO: [Mykyta]: 
+# Create python program which accepts input string `s`, 
+# `start` index, `end` index and prints substring (from start to end) to console in `string_slice.py`.
+# So "string_slice" is `s`, `start` is "0", `end` index is "2" in your case, just accept those as input as well :)
