365ba8d6eb988cf4b47eeae19e8fa234629ac2e0
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-20 23:10:48 +0200

    change request (task 9)

--- week_1/contains/contains.py
+++ week_1/contains/contains.py
@@ -4,4 +4,8 @@ if __name__ == '__main__':
 word = input('Enter word: ')
 letter = input('Enter letter: ')
 
+# TODO: [Mykyta]: what about built-in operators? (https://realpython.com/python-in-operator/)
+# Using magic methods (with double underscore) directly is sort of "dark magic",
+# which should not be used unless you absolutely there is no other way.
+# Extra useful reading (https://realpython.com/python-string-contains-substring/)
 print(word.__contains__(letter))
