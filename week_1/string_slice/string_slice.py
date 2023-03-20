2249e87a458591ff51084069a4ca6ef338d70f7f
ntestdan <ntestdan@gmail.com>
2023-03-20 21:14:28 +0200

    Update string_slice.py
    add program which slice string

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+
+s = input('Enter string: ')
+start = int(input('Enter start index: '))
+end = int(input('Enter end index: '))
+
+print(s[start:end])
