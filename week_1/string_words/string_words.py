3cce2d8fc3aa0c08158b0ee4403ddc8c13f283d3
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-20 23:34:19 +0200

    change request (task 6)

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -1,5 +1,11 @@
 if __name__ == '__main__':
-    print('Task 6. String words')  # TODO: [Mykyta] Why there is a blank space?)
+    print('Task 6. String words')  # TODO: [Mykyta] Why there is a blank space only here?)
+# TODO: [Mykyta] We will cover that a bit later, but in Python, 
+# each line should have 4 spaces inside `if block`. 
+# It means, this code should be inkoke if condition is True. 
+# `if __name__ == '__main__'` is True when you run it directly. 
+# The problem with your code may occure when somebody will import it to their programs. 
+# So for now, stick with blank spaces at each line.
 f = "It is better to take many small steps\
 in the right direction than to make a great leap\
 forward only to stumble backward."
