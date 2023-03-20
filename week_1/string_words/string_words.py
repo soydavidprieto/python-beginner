8828bf270c6bb0434dc1cbea4a00585133ccf314
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-20 23:28:42 +0200

    change request (task 6)

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
-    print('Task 6. String words')
+    print('Task 6. String words')  # TODO: [Mykyta] Why there is a blank space?)
 f = "It is better to take many small steps\
 in the right direction than to make a great leap\
 forward only to stumble backward."
-print(f)
+print(f) # TODO: [Mykyta] Seems you didn't split it into words yet, right?)
