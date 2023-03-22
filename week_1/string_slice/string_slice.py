e620fc43ab846b1548bb041e25305d20ead556a6
Andriy Havryshkevych <gavandre@gmail.com>
2023-03-22 10:22:24 +0200

    fix

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,9 +1,8 @@
-string_slice = input("Enter your string: ")
-print(string_slice[0])
-print(string_slice[-1])
-print(string_slice[0:])
-sub_string = string_slice[0::2]
-print(sub_string)
+s = input("Enter your string: ")
+start = int(input("Enter your start index: "))
+end = int(input("Enter your end index: "))
+print(s[start:end])
+
 
 #to be honest I haven't understood the task since it has very unclear explanation so I've just played around with slicing
 # TODO: [Mykyta]: 
--- week_1/sum_numbers/sum_numbers.py
+++ week_1/sum_numbers/sum_numbers.py
@@ -1,5 +1,3 @@
-
-
 while True:
     while True:
         x = input("Please enter your first number: ")
