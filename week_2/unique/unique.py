4a83a56bcf427367a482669ca0effce389a8251e
ntestdan <ntestdan@gmail.com>
2023-03-23 10:32:24 +0200

    Update unique.py
    unique (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+
+inputs_list = input('Input:')
+split_list = inputs_list.split(',')
+int_list = list(map(int, split_list))
+
+my_set = set((int_list))
+
+print('Unique:', my_set)
