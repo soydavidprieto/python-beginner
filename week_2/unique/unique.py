53fe94e3675447d862f288af2a5283097f108bf5
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-23 21:08:55 +0200

    minor change (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -6,5 +6,7 @@ split_list = inputs_list.split(',')
 int_list = list(map(int, split_list))
 
 my_set = set((int_list))
+# [Mykyta]: set(int_list), no extra brackets required, 
+# python ignores those (1) is 1, but (1,) != 1, because (1,) a tuple in that case.
 
 print('Unique:', my_set)
