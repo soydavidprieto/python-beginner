91e6b6fa20bf29b4896052f0284beef7d2cba82b
ntestdan <ntestdan@gmail.com>
2023-03-23 10:15:56 +0200

    Update contact.py
    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,16 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+
+
+x = input('Enter your name:')
+y = int(input('Enter your age:'))
+z = input('Enter your address:')
+c = input('Enter your phone:')
+
+thisdict = {
+    "name": x,
+    "age": y,
+    "address": z,
+    "phone": c
+}
+print('Contact created', thisdict)
