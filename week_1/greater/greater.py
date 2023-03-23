7f53c3ca535463e4bd6325fc7fae3178dea52aca
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-23 21:34:00 +0200

    change request (task 8)

--- week_1/greater/greater.py
+++ week_1/greater/greater.py
@@ -2,6 +2,6 @@
 x = input('Enter number 1: ')
 y = input('Enter number 2: ')
 if int(y) > int(x):
-    print ('y is greater then x')
+    print ('y is greater then x')  # [Mykyta] TODO: `print (` -> never place space between, it works, but it's a bad practice
 else:
     print ('x is greater then y')
