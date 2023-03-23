34d71f45c70738ec505d0b062d87b370fbf6f4ce
ntestdan <ntestdan@gmail.com>
2023-03-23 09:06:13 +0200

    Update each_third.py
    each third (task 11)

--- week_2/each_third/each_third.py
+++ week_2/each_third/each_third.py
@@ -1,2 +1,17 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
+
+
+inputs_list = input('Enter sequence of characters:')
+split_list = inputs_list.split(',')
+
+i = 0
+new_list = list()
+while i < len(split_list):
+    new_list.append(split_list[i])
+    i = i + 2
+
+print('Result list', new_list)
+
+
+
