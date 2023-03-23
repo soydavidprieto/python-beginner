29774733669de42f76baba68a90267b7b5ed7351
natalitaras <n.puchka@gmail.com>
2023-03-23 20:22:24 +0200

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    print('Task 14. Contact.')
+name = input('Enter your name:' )
+age = int (input('Enter your age:'))
+address = input('Enter your address:' )
+phone = int(input ('Enter your phone:'))
+my_list = [name, age, address, phone]
+print('Contact created:', my_list)
