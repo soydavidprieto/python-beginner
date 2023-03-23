686280be46899aab7d8f1eca71e6923922aac61a
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-23 21:22:23 +0200

    change request (task 6)

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -1,2 +1,6 @@
 text=input('Type text:' )
 print(text)
+
+# [Mykyta]: Sorry, the task is not very clear, but we need to know separate words here.
+# Take a look:
+# https://realpython.com/python-string-split-concatenate-join/
