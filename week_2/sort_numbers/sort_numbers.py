1365447c39de01c451fc7aac97e6eff461757cd2
ntestdan <ntestdan@gmail.com>
2023-03-23 08:36:07 +0200

    Update sort_numbers.py
    sort numbers (task 10)

--- week_2/sort_numbers/sort_numbers.py
+++ week_2/sort_numbers/sort_numbers.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 10. Sort numbers')
+
+inputs_list = input('Enter list of digits:')
+split_list = inputs_list.split(',')
+int_list = list(map(int, split_list))
+int_list.sort()
+
+print('Ordered list:', int_list)
+
