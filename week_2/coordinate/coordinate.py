a2ad0317d28f1a69ccea920dbc823ef459b1e386
ntestdan <ntestdan@gmail.com>
2023-03-23 10:07:43 +0200

    Update coordinate.py
    coordinate (task 13)

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+z = int(input('Enter Z:'))
+
+coordinate_tuple = (x, y, z)
+
+print('Coordinate', coordinate_tuple)
