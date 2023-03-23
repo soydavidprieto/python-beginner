6ce82cd9ba772fd6faad263a6d6ee39ce5cf7ab1
natalitaras <n.puchka@gmail.com>
2023-03-23 20:45:34 +0200

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -2,5 +2,5 @@ name = input('Enter your name:' )
 age = int (input('Enter your age:'))
 address = input('Enter your address:' )
 phone = int(input ('Enter your phone:'))
-my_list = [name, age, address, phone]
-print('Contact created:', my_list)
+my_dict = {'name': name, 'age': age, 'address': address, 'phone': phone}
+print('Contact created:', my_dict)
