ba1490296d655ccd631980534c337f0b8f58ba33
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-23 21:18:41 +0200

    change request (task 7)

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,3 +1,11 @@
 s=input('Type s:' )
 result=slice(2,4)
 print(s[result])
+
+# [Mykyta]: Interesting, but not exactly what we want to learn :)
+# (the task was to get a bit more familiar with slices using step)
+# FYI, https://realpython.com/lessons/string-slicing/
+# """
+# Create python program which accepts input string `s`, 
+#    `start` index, `end` index and prints substring (from start to end) to console in `string_slice.py`.
+# """
