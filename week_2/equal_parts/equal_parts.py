0d2b85c16bb9e7f42cd20da8f8c4ebef17e312ff
natalitaras <n.puchka@gmail.com>
2023-03-24 15:21:56 +0200

    equal parts (task 12)

--- week_2/equal_parts/equal_parts.py
+++ week_2/equal_parts/equal_parts.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    print('Task 12. Equal parts')
+sequence = list(input('Enter a sequence of characters:'))
+my_list_lenght=len(sequence)
+half_lenght=int(my_list_lenght/2)
+first_half=sequence[:half_lenght]12
+second_half=sequence[half_lenght:]
+print(first_half, second_half)
