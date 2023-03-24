c24d6cfe1c15ffe7208c1557d2e5d860b539a883
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 21:22:54 +0100

    add program which splits text into words

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
 # `if __name__ == '__main__'` is True when you run it directly. 
 # The problem with your code may occure when somebody will import it to their programs. 
 # So for now, stick with blank spaces at each line.
-f = "It is better to take many small steps\
-in the right direction than to make a great leap\
-forward only to stumble backward."
-print(f) # TODO: [Mykyta] Seems you didn't split it into words yet, right?)
+    my_string = 'Hello World'
+    string_words = 'H\ne\nl\nl\no\n''\nW\no\nr\nl\nd'
+    print(string_words)
+
+ # TODO: [Mykyta] Seems you didn't split it into words yet, right?)
