97d8822a40323dd0918f4cc1908d5d40596806aa
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 20:41:45 +0100

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
     contact = {}
-    contact['name'] =input('Enter your name:')
+    contact['name'] = input('Enter your name:')
     contact['age'] = input('Enter your age:')
     contact['address'] = input('Enter your address:')
     contact['phone'] = input('Enter your phone:')
