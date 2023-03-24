33be9d57cfbdbf0bc903ec3a51c5cc68e3fa5582
Andriy Havryshkevych <gavandre@gmail.com>
2023-03-24 15:57:00 +0200

    updated

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,5 +1,6 @@
 string_list = []
 int_list = []
+amount = {}
 while True:
     numbers = input("Enter your numbers separated by space or comma: ")
     if "," in numbers:
@@ -13,7 +14,17 @@ while True:
         print("Please enter you data correctly! ")
         continue
     my_set = set(int_list)
-    int_list = list(my_set)
-    int_list.sort()
-    print(int_list)
+    list_unique = list(my_set)
+    list_unique.sort()
+    print(list_unique)
+    for i in string_list:
+        amount[i] = string_list.count(i)
+    print(amount)
+    occurrences = 0
+    for x, y in amount.items():
+        if y > 1:
+            occurrences = occurrences + 1
+    print(f"The occurrences are enumerated:   {amount}" )
+    print(f"Tme amount of elements that are occurring is   {occurrences}")
     break
+
