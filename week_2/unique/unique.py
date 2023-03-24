a18251d41850a90ecf0eeb976023b15686c03672
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 19:25:53 +0100

    unique (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,2 +1,18 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+    sequence_of_numbers = [1, 2, 11, 1, 2, 2, 3]
+
+    #unique_list = []
+   # for x in y:
+      #  if x not in unique_list:
+     #      unique_list.append(x)
+
+    #for x in unique_list:
+    #   print(x)
+    unique_numbers = set(sequence_of_numbers)
+    unique_list = list(unique_numbers)
+    print(sorted(unique_list))
+
+
+
+
