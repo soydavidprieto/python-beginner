ec8f05ff89d8d5efe9cfa7a80170bd0804ef2559
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 21:39:23 +0100

    add program which splits text into words

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 # `if __name__ == '__main__'` is True when you run it directly. 
 # The problem with your code may occure when somebody will import it to their programs. 
 # So for now, stick with blank spaces at each line.
-    my_string = 'Hello World'
-    string_words = 'H\ne\nl\nl\no\n''\nW\no\nr\nl\nd'
-    print(string_words)
+    my_string = input('Enter string: ')
+    my_string = my_string.split(' ')
+    print('\n'.join(my_string))
 
  # TODO: [Mykyta] Seems you didn't split it into words yet, right?)
