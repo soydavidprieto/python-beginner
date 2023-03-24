de2612dbd3fb6f058ea9b32f04f0f01c8e842ba7
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 20:39:51 +0100

    coordinate (task 13)

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+    x = input('Enter x: ')
+    y = input('Enter y: ')
+    z = input('Enter z: ')
+    coordinate = (int(x), int(y), int(z))
+    print(f'Output: Coordinate: {coordinate}')
