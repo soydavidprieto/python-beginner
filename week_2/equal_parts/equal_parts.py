5f5a0f16218b4e3c946a4f56d21d510273b6489c
Світлана Петрущак <svitlana@MacBook-Air-Svitlana.local>
2023-03-24 21:06:37 +0100

    equal parts (task 12)

--- week_2/equal_parts/equal_parts.py
+++ week_2/equal_parts/equal_parts.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+    whole_list = [3, 2, 1, 4, 5,  2, 'ss', 'q']
+    length = int(len(whole_list)/2)
+    print(whole_list[:length])
+    print(whole_list[-length:])
+
+
