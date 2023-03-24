70f1434fcd7540838dae8fea90a84ccfe200fb82
Andriy Havryshkevych <gavandre@gmail.com>
2023-03-24 10:54:13 +0200

    contact program

---
+++ week_2/contact/contact.py
@@ -0,0 +1,10 @@
+contact = {"name": "sample", "age": "sample", "address": "sample", "phone": "sample"}
+name = input("Enter your name: ")
+contact["name"] = name
+age = input("Enter your age: ")
+contact["age"] = age
+address = input("Enter your address: ")
+contact["address"] = address
+phone = input("Enter your phone: ")
+contact["phone"] = phone
+print(contact)
