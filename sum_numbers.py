f86546e212dc14cf6632b2e6296454a7502475ef
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-25 19:30:29 +0200

    add program which sums two numbers

---
+++ sum_numbers.py
@@ -0,0 +1,7 @@
+if __name__=='__main__':
+    x = input('x: ')
+    y = input('y: ')
+    sum=float(x)+float(y)
+    print('Sum = ', sum)
+
+    #strange result in case x=1.1 and y=-5.99, sum = -4.890000000000001
