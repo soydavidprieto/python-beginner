9b9fa2b4aad80d76bfc9f8acada1d836d7837237
oliastanitska <oliastanitska@gmail.com>
2023-03-26 23:40:49 +0300

    coordinate (task 13)

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    print('Task 13. Coordinate.')
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+z = int(input('Enter z: '))
+coordinate = (x, y, z)
+print('Coordinate', coordinate)
+
+
+
+
