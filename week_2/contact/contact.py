f93fd4d04daebd95ff71c2e744fa0ae796497224
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:29:51 +0300

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+    my_dict={}
+    n = input('Enter your name: ')
+    age = input('Enter your age: ')
+    address = input('Enter your address: ')
+    ph = input('Enter your phone: ')
+
+    my_dict = dict(name=n, age=age, address=address, phone=ph)
+    print("Contact created: ", my_dict)
