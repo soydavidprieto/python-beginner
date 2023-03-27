32c549a953e69ff88362a88f05da6b8ccd76c8e4
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:20:17 +0300

    check if number is greater

--- week_1/greater/greater.py
+++ week_1/greater/greater.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    print('Task 8. Is greater?')
+if __name__=='__main__':
+    x = input('x: ')
+    y = input('y: ')
+
+    if y > x:
+        print('y is greater than x so we display x as a result: ', x)
