ba4395df4c2c2ae548798a39d4da31771e585157
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:31:02 +0300

    coordinate (task 13)

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+    my_list=[]
+    x = input('Enter x: ')
+    y = input('Enter y: ')
+    z = input('Enter z: ')
+    my_list.append(x)
+    my_list.append(y)
+    my_list.append(z)
+    print("Coordinate ", my_list)
