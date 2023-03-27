392f8c868651a786f5ff62d69148aa55749fd5fc
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:24:25 +0300

    add program which slice string

--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+    s = input('Type any string: ')
+    start = input('Start symbol: ')
+    end = input('End symbol: ')
+    substring=s[int(start):int(end)]
+    print("substring(from start to end) = ", substring)
