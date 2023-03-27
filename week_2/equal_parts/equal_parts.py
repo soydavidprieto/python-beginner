af298120c17c65f8139f44557e13bd1b7bfc32db
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:33:23 +0300

    equal parts (task 12)

--- week_2/equal_parts/equal_parts.py
+++ week_2/equal_parts/equal_parts.py
@@ -1,2 +1,11 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+    my_list=[3, 2, 1, 4, 5,  2, "ss", "q", 9]
+
+    my_list_length = len(my_list)
+    n=int(my_list_length/2)
+
+    my_list2=my_list[0:n]
+    my_list3=my_list[n:]
+
+    print(my_list2, my_list3)
