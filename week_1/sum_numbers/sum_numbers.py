e6835dfab5fac34d82352f3d71cbf48f620788ee
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 20:27:59 +0300

    add program which sums two numbers

--- week_1/sum_numbers/sum_numbers.py
+++ week_1/sum_numbers/sum_numbers.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 3. Sum numbers.')
+    x = input('x: ')
+    y = input('y: ')
+    sum=float(x)+float(y)
+    print('Sum = ', sum)
+
+    #strange result in case x=1.1 and y=-5.99, sum = -4.890000000000001
