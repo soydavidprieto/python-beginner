e3d44e2bd04f79f4b2b366443788d8c19923127f
Iryna Karchevska <ir.karchevska@gmail.com>
2023-03-27 21:39:21 +0300

    movie_picker (task 16)

--- week_3/movie_picker/movie_picker.py
+++ week_3/movie_picker/movie_picker.py
@@ -10,20 +10,31 @@ if __name__ == '__main__':
         'action': ['Mission Impossible']
     }
 
-    ACTORS = {
-        'Robert De Niro': ['Meet the Parents'],
-        'Ben Stiller': ['Meet the Parents'],
-        'Adam Sandler': ['Anger Management'],
-        'Jack Nicholson': ['Anger Management'],
-        'Brendan Fraser': ['Mummy'],
-        'Rachel Weisz': ['Mummy'],
-        'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
-        'Penelope Cruz': ['Vanilla Sky'],
-        'Cameron Diaz': ['Vanilla Sky'],
-        'Brad Pitt': ['Meet Joe Black'],
-        'Anthony Hopkins': ['Meet Joe Black'],
-        'Jeremy Renner': ['Mission Impossible']
+    # (!) ACTORS storage does not exist anymore.
+    # ACTORS = {
+    #     'Robert De Niro': ['Meet the Parents'],
+    #     'Ben Stiller': ['Meet the Parents'],
+    #     'Adam Sandler': ['Anger Management'],
+    #     'Jack Nicholson': ['Anger Management'],
+    #     'Brendan Fraser': ['Mummy'],
+    #     'Rachel Weisz': ['Mummy'],
+    #     'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
+    #     'Penelope Cruz': ['Vanilla Sky'],
+    #     'Cameron Diaz': ['Vanilla Sky'],
+    #     'Brad Pitt': ['Meet Joe Black'],
+    #     'Anthony Hopkins': ['Meet Joe Black'],
+    #     'Jeremy Renner': ['Mission Impossible']
+    # }
+
+    CAST = {
+        'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+        'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+        'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+        'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+        'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+        'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
     }
+    available_actors=[]
 
     search_by_genre = input('Search by Genre? y/n: ')
 
@@ -36,10 +47,13 @@ if __name__ == '__main__':
         print('Movie to watch:', movie, "Genre: ", genre)
     elif search_by_genre == 'n':
         search_by_actor = input('Search by Actor? y/n: ')
-        print('Available Actors:', list(ACTORS.keys()))
+        for key, val in CAST.items():
+            for i in val:
+                available_actors.append(i)
+        print('Available Actors:', available_actors)
         actor = input('Enter actor: ')
-        available_movies = {key: value for (key, value) in ACTORS.items() if actor == key}
-        print('Available Movies:', list(available_movies.values()))
+        available_movies = {key: value for (key, value) in CAST.items() if str(actor) in str(value)}
+        print('Available Movies:', list(available_movies.keys()))
         movie = input('Enter movie: ')
         print('Movie to watch:', movie, "Starring: ", actor)
     else:
