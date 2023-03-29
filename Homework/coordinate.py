034c75d271b91e7f16a1525fc403541d1c4ca0e2
mykytapavlov <mykytapavlov@users.noreply.github.com>
2023-03-29 22:51:40 +0300

    change request

--- Homework/coordinate.py
+++ Homework/coordinate.py
@@ -2,3 +2,5 @@ x = input("Enter x coordinate: ")
 y = input("Enter y coordinate: ")
 z = input("Enter z coordinate: ")
 print(f'Coordinate: ({x}, {y}, {z})')
+
+# TODO: [Mykyta] In this task you should use tuple as a container for x, y, z
