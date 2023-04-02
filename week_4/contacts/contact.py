d1460ad9696ce6ef549d9d3307d996421bd77db2
ntestdan <ntestdan@gmail.com>
2023-04-02 21:40:27 +0300

    Update contact.py
    Task#19.1

--- week_4/contacts/contact.py
+++ week_4/contacts/contact.py
@@ -26,5 +26,31 @@ def validate_age(age):
 
 
 class Contact:
-    # TODO: implement instead of above
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    def validate_name(self):
+        if len(self.name) > 50:
+            raise ValueError('Name is too large!')
+
+    def validate_email(self):
+        if '@' not in self.email or '.' not in self.email:
+            raise ValueError('Invalid email!')
+
+    def validate_age(self):
+        try:
+            self.age = int(self.age)  # Python will raise ValueError if not numeric
+            if self.age <= 0:
+                # We ask Python to raise ValueError if <= 0
+                raise ValueError
+        except ValueError:
+            raise ValueError('Invalid age!')
+
+
+
+    def __str__(self):
+        return f"added contact: ({self.name},{self.email},{self.age})"
+
     ...
