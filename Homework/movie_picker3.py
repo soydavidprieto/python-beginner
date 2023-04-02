9bf00f82d2c7401c4cf416d38d1ee8aae1877fc4
Joanna Langwerska <jlangwerska@gmail.com>
2023-04-02 14:03:57 +0200

    Correction

--- Homework/movie_picker3.py
+++ Homework/movie_picker3.py
@@ -32,49 +32,50 @@ CAST = {
     'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
 }
 
-search_type = ''
-while search_type != 'y' or 'n':  # TODO: [Mykyta] this is hard to read -> while search_type in {'y', 'n'}
-    search_type = input('Search by genre (y or n): ')
-    if search_type == "y":
-        print(f'Available genres: {GENRES.keys()}')
+search_type = input('Search by genre (y or n): ')
+while search_type not in {'y' , 'n'}:  # TODO: [Mykyta] this is hard to read -> while search_type in {'y', 'n'} [Joanna] Corrected
+    print("Wrong answer!")
+    search_type = input('Do you want to search by genre? (y or n): ')
+if search_type == "y":
+    print(f'Available genres: {GENRES.keys()}')
+    genre = input('Enter genre: ')
+    while genre not in GENRES.keys(): # TODO: [Mykyta] -> while genre not in GENRES; `in` checks keys by default. [Joanna] Doesnt work for me, there need to be .keys to check the keys.
+        print(f'Genre {genre} not found. Please try again.')
         genre = input('Enter genre: ')
-        while genre not in GENRES.keys(): # TODO: [Mykyta] -> while genre not in GENRES; `in` checks keys by default
-            print(f'Genre {genre} not found. Please try again.')
-            genre = input('Enter genre: ')
-        print(f'Avalaible movies: {GENRES[genre]}')
+    print(f'Avalaible movies: {GENRES[genre]}')
+    movie = input('Enter movie: ')
+    while movie not in GENRES[genre]:
+        print(f'Movie {movie} not found. Please try again.')
         movie = input('Enter movie: ')
-        while movie not in GENRES[genre]:
-            print(f'Movie {movie} not found. Please try again.')
-            movie = input('Enter movie: ')
-        print(f'Movie to watch: {movie}. Genre: {genre}.')
+    print(f'Movie to watch: {movie}. Genre: {genre}.')
 
-    elif search_type == "n":
-        search_actor = ''
-        while search_actor != 'y' or 'n':
-            search_actor = input('Search by actor (y or n): ')
-            if search_actor == "y":
-                actors_list = []
-                for actors in CAST.values():
-                    actors_list += actors
-                    unique_actors = set(actors_list)
-                print(f'Available Actors: {unique_actors}')
+elif search_type == "n":
+    search_actor = ''
+    while search_actor != 'y' or 'n':
+        search_actor = input('Search by actor (y or n): ')
+        if search_actor == "y":
+            actors_list = []
+            for actors in CAST.values():
+                actors_list += actors
+                unique_actors = set(actors_list)
+            print(f'Available Actors: {unique_actors}')
+            actor = input('Enter actor: ')
+            while actor not in unique_actors:
+                print(f'Actor {actor} not found. Please try again.')
                 actor = input('Enter actor: ')
-                while actor not in unique_actors:
-                    print(f'Actor {actor} not found. Please try again.')
-                    actor = input('Enter actor: ')
-                movies_list = []
-                for value in CAST.values():
-                    if actor in value:
-                        keys = [k for k, v in CAST.items() if v == value]
-                        movies_list += keys
-                print(f'Avalaible movies: {movies_list}')
+            movies_list = []
+            for value in CAST.values():
+                if actor in value:
+                    keys = [k for k, v in CAST.items() if v == value]
+                    movies_list += keys
+            print(f'Avalaible movies: {movies_list}')
+            movie = input('Enter movie: ')
+            while movie not in movies_list:
+                print(f'Movie {movie} with actor {actor} not found. Please try again.')
                 movie = input('Enter movie: ')
-                while movie not in movies_list:
-                    print(f'Movie {movie} with actor {actor} not found. Please try again.')
-                    movie = input('Enter movie: ')
-                print(f'Movie to watch: {movie}. Starring: {actor}.')
-                break
-            else:
-                print("OK, good bye!")
-                break
-    break
+            print(f'Movie to watch: {movie}. Starring: {actor}.')
+            break
+        else:
+            print("OK, good bye!")
+            break
+
