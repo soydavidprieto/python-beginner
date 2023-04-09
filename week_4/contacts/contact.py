094fa12424c4ba9ec9f1d89f5373d4633e068c89
Iryna Karchevska <ir.karchevska@gmail.com>
2023-04-09 18:03:16 +0300

    Task 19. Part 1

--- week_4/contacts/contact.py
+++ week_4/contacts/contact.py
@@ -1,30 +1,21 @@
-contact = {
-    'name': None,
-    'email': None,
-    'age': None
-}
-
-
-def validate_name(name):
-    if len(name) > 50:
-        raise ValueError('Name is too large!')
-
-
-def validate_email(email):
-    if '@' not in email or '.' not in email:
-        raise ValueError('Invalid email!')
-
-
-def validate_age(age):
-    try:
-        age = int(age)  # Python will raise ValueError if not numeric
-        if age <= 0:
-            # We ask Python to raise ValueError if <= 0
-            raise ValueError
-    except ValueError:
-        raise ValueError('Invalid age!')
-
-
 class Contact:
-    # TODO: implement instead of above
-    ...
+
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+        if len(self.name) > 50:
+            raise ValueError('Name is too large!')
+
+        if '@' not in self.email or '.' not in self.email:
+            raise ValueError('Invalid email!')
+
+        try:
+            self.age = int(self.age)  # Python will raise ValueError if not numeric
+            if self.age <= 0:
+                # We ask Python to raise ValueError if <= 0
+                raise ValueError
+        except ValueError:
+            raise ValueError('Invalid age!')
+
--- week_4/contacts/main.py
+++ week_4/contacts/main.py
@@ -1,36 +1,36 @@
-from contact import contact, validate_name, validate_age, validate_email
+from contact import Contact
 
+print('Task 19. Contact List')
 
-if __name__ == '__main__':
-    print('Task 19. Contact List')
+contact = {
+    'name': None,
+    'email': None,
+    'age': None
+}
 
-    contact_list = []
+contact_list = []
 
-    while True:
-        try:
-            name = input('name: ')
-            validate_name(name)
+while True:
+    try:
+        name = input('name: ')
+        email = input('email: ')
+        age = input('age: ')
+    except ValueError as e:
+        print(e)
+        continue
+        
+    contact_inst = Contact(name, email, age)
+    new_contact = contact.copy()
+    new_contact['name'] = contact_inst.name
+    new_contact['email'] = contact_inst.email
+    new_contact['age'] = contact_inst.age
 
-            email = input('email: ')
-            validate_email(email)
+    print('-' * 10)
+    print(new_contact)
+    print('-' * 10)
 
-            age = input('age: ')
-            validate_age(age)
-        except ValueError as e:
-            print(e)
-            continue
+    contact_list.append(new_contact)
+    proceed = input('add another one? (y/n): ')
 
-        new_contact = contact.copy()
-        new_contact['name'] = name
-        new_contact['email'] = email
-        new_contact['age'] = age
-
-        print('-' * 10)
-        print(new_contact)
-        print('-' * 10)
-
-        contact_list.append(new_contact)
-        proceed = input('add another one? (y/n): ')
-
-        if proceed != 'y':
-            break
+    if proceed != 'y':
+        break
