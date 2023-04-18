de42438a110d7edd0888b9d8c868e63fab2c19ce
ntestdan <ntestdan@gmail.com>
2023-04-18 21:41:57 +0300

    Update main.py
    fixed

--- week_4/contacts/main.py
+++ week_4/contacts/main.py
@@ -11,14 +11,10 @@ if __name__ == '__main__':
         email = input('email: ')
         age = input('age: ')
 
-        entered_contact = Contact(name, email, age)
-
-        try:
-            entered_contact.validate_name()
-            entered_contact.validate_email()
-            entered_contact.validate_age()
-        except ValueError as e:
-            print(e)
+        entered_contact = Contact()
+        entered_contact.name = name
+        entered_contact.age = age
+        entered_contact.email = email
 
         new_contacts.append(entered_contact)
         print(entered_contact)
