0f95eb989d9cbcde47c0d402ba2a49b146ef9c90
Iryna Karchevska <ir.karchevska@gmail.com>
2023-04-20 21:14:50 +0300

    task 101

--- week_5/snake/snake.py
+++ week_5/snake/snake.py
@@ -38,18 +38,41 @@ class Board:
 
 
 class Snake:
-    def __init__(self, body='o', position=(1, 1)):
-        self.body = body
-        self.position = position
+    def __init__(self, symbol='o', position=(1, 1)):
+        self.symbol = symbol
+        self.body = [position]
+
+    def eat(self, position: tuple):
+        self.body.append(position)
+
+    def move(self, position: tuple):
+        del(self.body[0])
+        self.body.append(position)
 
 
 class Game:
     def __init__(self):
-        self.board = Board(width=4, height=4)
+        self.board = Board(width=20, height=20)
         self.snake = Snake()
 
     def render(self):
-        ...
+        # the easiest way to place snake's body on the board
+        i, j = self.snake.body[0]
+        self.board.board[i][j] = self.snake.symbol
+
+        apple = (1, 2)
+        self.snake.eat(apple)
+        self.board.board[apple[0]][apple[1]] = self.snake.symbol
+
+        apple = (2, 2)
+        self.snake.eat(apple)
+        self.board.board[apple[0]][apple[1]] = self.snake.symbol
+
+        apple = (2, 3)
+        self.snake.eat(apple)
+        self.board.board[apple[0]][apple[1]] = self.snake.symbol
+
+        self.board.show()
 
 
 if __name__ == '__main__':
@@ -61,3 +84,12 @@ if __name__ == '__main__':
     #     print(cur_row)
 
     b.show()
+    s = Snake()
+    print(s.body)
+    s.eat((1, 2))
+    print(s.body)
+
+    s.move((1, 3))
+    print(s.body)
+    game = Game()
+    game.render()
