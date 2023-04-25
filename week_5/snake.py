0ed971f572e463def0a9c690b101e1585e84e464
Andriy Havryshkevych <gavandre@gmail.com>
2023-04-25 18:34:08 +0300

    updates

--- week_5/snake.py
+++ week_5/snake.py
@@ -40,14 +40,48 @@ class Board:
 class Snake:
     def __init__(self, symbol='o', position=(1, 1)):
         self.symbol = symbol
-        self.position = position
+        self.body = [position]
+
+    def eat(self, position: tuple):
+        self.body.append(position)
+
+    def move(self, position: tuple):
+        self.body.append(position)
+        self.body.pop(-1)
 
 
 class Game:
     def __init__(self):
-        self.board = Board(width=4, height=4)
+        self.board = Board(width=10, height=10)
         self.snake = Snake()
 
-
-
     def render(self):
+        # the easiest way to place snake's body on the board
+        i, j = self.snake.body[0]
+        self.board.board[i][j] = self.snake.symbol
+        self.board.show()
+        x_coord = range(1, 10)
+        y_coord = range(1, 10)
+        apple_lst = []
+        #while True:
+        for x, y in zip(x_coord, y_coord):
+            apple_lst.append(x)
+            apple_lst.append(y)
+            apple = tuple(apple_lst)
+            self.snake.eat(apple)
+            apple_lst.pop(0)
+            apple_lst.pop(0)
+
+
+                # apple = (2, 2)
+                # self.snake.eat(apple)
+                #
+                # apple = (2, 3)
+                # self.snake.eat(apple)
+
+        self.board.show()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.render()
