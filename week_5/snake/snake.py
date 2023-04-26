0f42bd40c7812f85fa4225f68ece912ddfed1b94
ntestdan <ntestdan@gmail.com>
2023-04-26 20:46:30 +0300

    Update snake.py
    step#1

--- week_5/snake/snake.py
+++ week_5/snake/snake.py
@@ -31,6 +31,12 @@ class Board:
                 board.append(row)
         return board
 
+    def draw_snake(self, snake):
+        for snake_point in snake.body:
+            i, j = snake_point
+            self.board[i][j] = snake.symbol
+
+
     def show(self):
         for row in self.board:
             # print(row)
@@ -38,26 +44,74 @@ class Board:
 
 
 class Snake:
-    def __init__(self, body='o', position=(1, 1)):
-        self.body = body
-        self.position = position
+    def __init__(self, symbol='o', position=(1, 1)):
+        self.symbol = symbol
+        self.body = [position]
+
+    def eat(self, position: tuple):
+
+        # TODO: this method should append new position to snake's body
+        self.body.append(position)
+
+    def move(self, position: tuple):
+
+        # TODO: this method should move snake's body by one step forward,
+        # by means append to the body next position and
+        # pop from the body the position at 0 index at the same time.
+        self.body.append(position)
+        self.body.pop(0)
 
 
 class Game:
     def __init__(self):
-        self.board = Board(width=4, height=4)
+        self.board = Board(width=20, height=20)
         self.snake = Snake()
 
     def render(self):
-        ...
+        # the easiest way to place snake's body on the board
+        i, j = self.snake.body[0]
+        self.board.board[i][j] = self.snake.symbol
+
+        apple = (1, 2)
+        self.snake.eat(apple)
+
+        apple = (2, 2)
+        self.snake.eat(apple)
+
+        apple = (2, 3)
+        self.snake.eat(apple)
+
+        apple = (2, 4)
+        self.snake.eat(apple)
+
+        apple = (3, 4)
+        self.snake.eat(apple)
+
+        # TODO: your task is to update self.board.board here to render entire body of the snake now
+        self.board.draw_snake(self.snake)
+
+        self.board.show()
 
 
 if __name__ == '__main__':
-    b = Board(width=4, height=4)
+    game = Game()
+    game.render()
 
-    # lst = b.init_board()
+
+    # b = Board(width=4, height=4)
     #
-    # for cur_row in lst:
-    #     print(cur_row)
+    # # lst = b.init_board()
+    # #
+    # # for cur_row in lst:
+    # #     print(cur_row)
+    #
+    # b.show()
+    #
+    # s = Snake()
+    # s.eat((1, 2))
+    # print(s.body)
+    # # [(1, 1), (1, 2)]
 
-    b.show()
+    # s.move((1, 3))
+    # print(s.body)
+    # # [(1, 2), (1, 3)]
