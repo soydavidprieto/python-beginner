6afb82ec95656a7380bdc4ae518e7c5d496dc61b
Olena lesyk <osafo1985@gmail.com>
2023-06-13 10:34:55 +0200

    add program which splits text into words

--- week_1/string_words/string_words.py
+++ week_1/string_words/string_words.py
@@ -1,2 +1,7 @@
-if __name__ == '__main__':
-    print('Task 6. String words')
+txt = "The ways we work at SoftServe change. " \
+    "We noticed that you rarely use telephones to " \
+    "make calls or join conferences. We are considering " \
+    "the option of removing this service. Please " \
+    "help us make a thoughtful decision and let us know:"
+x = txt.split()
+print(x)
