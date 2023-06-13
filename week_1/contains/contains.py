f61b33a2f2c540bad0b573885c8a8935da581664
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-13 15:40:53 +0300

    add program which slice string

--- week_1/contains/contains.py
+++ week_1/contains/contains.py
@@ -1,2 +1,5 @@
 if __name__ == '__main__':
     print('Task 9. Contains?')
+    a='word'
+    b='letter'
+    print('a' in 'b')
--- week_1/string_slice/string_slice.py
+++ week_1/string_slice/string_slice.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+    x=input('s:')
+    start=input('start')
+    start=int(start)
+    end=input('end')
+    end=int(end)
+    result=x[start:end]
+    print(result)
+
