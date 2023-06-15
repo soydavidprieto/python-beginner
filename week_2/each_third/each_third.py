fd29ed4e8a7bdee042df70b5b9f0c252afcdd846
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-15 12:45:46 +0300

    each third (task 11)

--- week_2/each_third/each_third.py
+++ week_2/each_third/each_third.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
     my_list = [4, 6, 9, 5, 7, 8]
-    print(my_list[2],my_list[5])
-    
+    print(my_list[0::2])
+    my_list = [3, 2, 1, 4, 5, 2, 'ss', 'q']
+    print(my_list[0::2])
+
