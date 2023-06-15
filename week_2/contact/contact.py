f9fba75064525e129b7d4218144664c59e08c611
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-15 12:53:40 +0300

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,9 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+    name = input()
+    age = input()
+    address = input()
+    phone = input()
+    my_dict = {'name': name, 'age': age, 'address': address , 'phone': phone}
+    print(f'Contact created: {my_dict}')
+    #print(f'Contact created: name:{name}, age:{age}, address:{address}, phone:{phone}')
