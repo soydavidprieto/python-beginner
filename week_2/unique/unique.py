b0f7fcb769a5dc73dba6840d0c100948e5e6f0c2
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-15 10:40:38 +0300

    unique (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+    numbers = [1, 2, 11, 1, 2, 2, 3]
+    unique_numbers = list(set(numbers))
+    sorted_numbers = unique_numbers.sort()
+    print(unique_numbers)
+
+
