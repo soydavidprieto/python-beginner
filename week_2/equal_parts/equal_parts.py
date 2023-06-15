99126a07c34cbe2ea6daf5ac38c5856ccbded087
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-15 12:47:32 +0300

    equal parts (task 12)

--- week_2/equal_parts/equal_parts.py
+++ week_2/equal_parts/equal_parts.py
@@ -1,8 +1,12 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
-    my_list = [1, 5, 7, 3, 6, 9, 2]
-    #print(len(my_list))
-    sub_sequence = my_list[:3]
+    my_list = [1, 5, 7, 3, 6, 9, 2, 6, 4]
+    my_list_length = len(my_list)
+    print(my_list_length)
+    #my_list1 = int(my_list_length/2)
+    my_list1 = my_list_length //2
+    print(my_list1)
+    sub_sequence = my_list[:my_list1]
     print(sub_sequence)
-    sub_sequence = my_list[-4:]
+    sub_sequence = my_list[my_list1:]
     print(sub_sequence)
