e3ce8fd0359797b79227d318e4b0cf47b387c829
Olena lesyk <osafo1985@gmail.com>
2023-06-16 10:54:16 +0200

    change the preious code so user can input

--- week_1/greater/greater.py
+++ week_1/greater/greater.py
@@ -1,3 +1,7 @@
-x = 6
-y = 1
-print(x > y)
+print('Hi! Lets check x and y what is greater')
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+if x > y:
+    print('x is the biggest')
+if x <= y:
+    print('y is the biggest (or even equal to x)')
