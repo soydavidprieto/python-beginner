cec54647bb1125f737a8a33e2fccc22b25b07496
David Prieto Chavarro <dprie@softserveinc.com>
2023-06-22 11:11:04 -0500

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    print('Task 14. Contact.')
+Name= input('Insert your name: ')
+Age = input('Insert your age: ')
+Address = input('Input your address: ')
+Phone = input('Insert your phone number: ')
+Contact = {'Name': Name, 'Age': Age, 'Address': Address, 'Phone': Phone}
+print(Contact['Name'], 'This is your information: ', Contact)
