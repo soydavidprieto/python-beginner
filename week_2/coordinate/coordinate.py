48f71b36248de1633b697f7542adcb790481730f
David Prieto Chavarro <dprie@softserveinc.com>
2023-06-22 10:29:36 -0500

    Edit the task

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,5 +1,5 @@
 x = input('Enter a coordinate for X: ')
 y = input('Enter a coordinate for Y: ')
 z = input('Enter a coordinate for Z: ')
-
-print('Coordinate ' ,'(', x, '), ', '(', x, ')', ', ', '(', z,')')
+Coordinate = list [x, y, z]
+print('Coordinate ' , Coordinate)
