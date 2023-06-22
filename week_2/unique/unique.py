0813ac4f1f458a9b4adeac846f374283a35b9bb9
David Prieto Chavarro <dprie@softserveinc.com>
2023-06-22 11:45:51 -0500

    unique (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,2 +1,5 @@
-if __name__ == '__main__':
-    print('Task 15. Unique.')
+Elements = input('Enter elements of a list separated by space ')
+print("\n")
+listOfElements = Elements.split()
+listOfElements.sort()
+print(listOfElements)  # [1, 2, 3, 4]
